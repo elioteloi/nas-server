ba8e3c601d8ffe73066d3a1bbac7e6eecf9ba4bb
Eliot Eloi <eliot.eloi22@gmail.com>
2025-01-14 13:31:34 -0300

    Add MySQL database connection configuration

---
+++ database/db.js
@@ -0,0 +1,18 @@
+let mysql = require("mysql2");
+require("dotenv").config();
+
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Connected to database");
+  }
+});
+module.exports = connection;
